b2c0acc0ec2b3318277e1728e74b8862bb1d0dee
Daniel Murilo Vaz <daniellmvaz@gmail.com>
2023-10-18 21:04:11 -0300

    update

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,25 +1,30 @@
-const client = require('../service/prisma')
-const data = require('../data/index.json')
+const client = require('../service/prisma');
+const data = require('../data/index.json');
+
+// req -> request
+// res -> response
 
 module.exports = {
   async index(req, res) {
-    const products = await client.product.findMany()
+    console.log(req.body);
+    // SELECT * FROM products
+    const products = await client.product.findMany();
 
-    return res.status(200).json(products)
+    return res.status(200).json(products);
   },
-  async findById(req, res) {
-    
-  },
+  async findById(req, res) {},
   async create(req, res) {
-    // Percorrer o array [data] e incluir todos os produtos
+    // Percorrer o array [data] e incluir todos os produtos na tabela
     // para incluir o produto usar a função [await client.product.create({ data: product })]
 
-    return res.status(201).json(data)
-  },
-  async update(productId, data) {
+    // crie um array e de um push para adicionar o produtor criado
 
-  },
-  async delete(productId) {
+    /**
+     *
+     */
 
-  }
-}
+    return res.status(201).json();
+  },
+  async update(req, res) {},
+  async delete(req, res) {},
+};
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,21 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
-const productsController = require('./controllers/product-controller')
+const productsController = require('./controllers/product-controller');
 
-const router = Router()
+const router = Router();
 
 /**
- * Metodos HTTP GET POST PUT PATCH e DELETE 
+ * Metodos HTTP GET POST PUT PATCH e DELETE
  */
 
-router.get('/products', productsController.index)
-router.get('/product/:id', productsController.findById)
-router.post('/product', productsController.create)
-router.put('/product/:id', productsController.update)
-router.delete('/product/:id', productsController.delete)
+// Get Buscar
+router.get('/products', productsController.index);
+router.get('/product/:id', productsController.findById);
+// Post Criar
+router.post('/product', productsController.create);
+// Put ou Patch atualizar
+router.put('/product/:id', productsController.update);
+// Delete deletar
+router.delete('/product/:id', productsController.delete);
 
 module.exports = router;
